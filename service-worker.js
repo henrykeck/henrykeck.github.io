abf19de05ee99a688e9b19726ffd23c432397183
Henry Johnson <hennrymjohnson@gmail.com>
2023-08-30 19:43:06 -0700

    missed a couple. Also trying another caching

--- service-worker.js
+++ service-worker.js
@@ -4,58 +4,40 @@ const DYNAMIC_CACHE_NAME = `dynamic-cache-v${CACHE_VERSION}`;
 const STATIC_ASSETS = [
     '/', // the main HTML file
     '/scripts.js',
-    '/styles.css', // Replace with your actual CSS path
-    '/artifacts/logos/logo.png',
-    '/artifacts/logos/logo-slim.png'
+    '/styles.css',
+    '/artifacts/logos/logo.webp',
+    '/artifacts/logos/logo-slim.webp',
+    '/artifacts/content-photos/filler/marble_wallpaper.webp',
+    '/artifacts/content-photos/filler/white_octagon_wallpaper.webp',
+    '/artifacts/content-photos/filler/white_octagon_wallpaper.webp',
+    '/artifacts/content-photos/headers/blue_stripe.webp',
+    '/artifacts/content-photos/headers/orange_stripe.webp',
+    '/artifacts/content-photos/headers/green_stripe.webp',
+    '/artifacts/content-photos/personnel/dave_headshot.webp',
+    '/artifacts/content-photos/personnel/dave_fun.webp',
+    '/artifacts/content-photos/wallcovering-types/wood_square.webp',
+    '/artifacts/content-photos/wallcovering-types/whiteboard_square.webp',
+    '/artifacts/content-photos/wallcovering-types/sisal_square.webp',
+    '/artifacts/content-photos/wallcovering-types/residential_square.webp',
+    '/artifacts/content-photos/wallcovering-types/panel_square.webp',
+    '/artifacts/content-photos/wallcovering-types/mural_square.webp',
+    '/artifacts/content-photos/wallcovering-types/foil_square.webp',
+    '/artifacts/content-photos/wallcovering-types/fabric_square.webp',
+    '/artifacts/content-photos/wallcovering-types/commercial_square.webp'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(STATIC_CACHE_NAME).then((cache) => {
-            return cache.addAll(STATIC_ASSETS);
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    const requestUrl = event.request.url;
-
-    // Check if the request URL matches any of the directories you want to cache
-    const isTargetedRequest = [
-        '/artifacts/content-photos/wallcovering-types',
-        '/artifacts/content-photos/personnel',
-        '/artifacts/content-photos/headers',
-        '/artifacts/content-photos/filler',
-        '/artifacts/wallcovering-type-descriptions'
-    ].some(directory => requestUrl.includes(directory));
-
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            if (cachedResponse) return cachedResponse;
-
-            // Cache the request if it's targeted
-            if (isTargetedRequest) {
-                return fetch(event.request).then(fetchResponse => {
-                    return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
-                        cache.put(event.request, fetchResponse.clone());
-                        return fetchResponse;
-                    });
-                });
-            }
-
-            // If it's not a targeted request, just forward it
-            return fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    const expectedCaches = [STATIC_CACHE_NAME, DYNAMIC_CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((keys) => 
-            Promise.all(keys.map(key => {
-                if (!expectedCaches.includes(key)) return caches.delete(key);
-            }))
-        )
-    );
-});
+    // Precache assets on install
+    event.waitUntil(caches.open(cacheName).then((cache) => {
+      return cache.addAll(precachedAssets);
+    }));
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    // Always try the cache for every request
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || Promise.reject('No match in cache');
+      });
+    }));
+  });
